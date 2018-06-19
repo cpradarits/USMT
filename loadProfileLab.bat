d3373ce5b416ae84f4a3942898d56644357257f8
cpradarits <cpradarits@users.noreply.github.com>
2018-06-19 13:46:32 -0500

    Add files via upload

---
+++ loadProfileLab.bat
@@ -0,0 +1,2 @@
+%~dp0\USMT\amd64\loadstate.exe %~dp0\store\Lab /i:%~dp0\USMT\amd64\MigApp.xml /i:%~dp0\USMT\amd64\MigUser.xml /ue:%computername%\* /uel:90 /v:13 /c /l:%~dp0\store\Lab\%COMPUTERNAME%-loadstate.log
+
---
+++ scanProfileLabs.bat
@@ -0,0 +1,1 @@
+%~dp0\USMT\amd64\scanstate.exe %~dp0\store\Lab /i:%~dp0\USMT\amd64\MigApp.xml /i:%~dp0\USMT\amd64\MigUser.xml /o /vsc /ue:%computername%\* /uel:90 /c /v:13 /l:%~dp0\store\Lab\scanstate.log
